6b221c854b835f635fb199a32cff43bf591c9ca2
LulaKao <windandsprings@gmail.com>
2019-12-06 17:48:12 +0800

    GridView 一排 4 個 OK

--- app/src/main/java/com/example/slideimage_gridview_test/MainActivity.java
+++ app/src/main/java/com/example/slideimage_gridview_test/MainActivity.java
@@ -3,21 +3,15 @@ package com.example.slideimage_gridview_test;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    private List<Integer> list = new ArrayList<>(4); // 4 張 banner
     RecyclerView grid_recycler;
     GridLayoutManager layoutManager;
     GridAdapter gridAdapter;
 
-    private int iconId[]={R.drawable.aa,R.drawable.bb,R.drawable.cc,R.drawable.dd,R.drawable.ee,R.drawable.ff};
-    private String nameList[]={"十股糖仁文創園區","國家歌劇院","傳統藝術中心","清水斷崖","奇美博物館","六福村"};
+    private int iconId[]={R.drawable.aa,R.drawable.bb,R.drawable.cc,R.drawable.dd,R.drawable.ee,R.drawable.ff,R.drawable.gg,R.drawable.hh,R.drawable.ii,R.drawable.jj,R.drawable.kk,R.drawable.ll};
+    private String nameList[]={"十股糖仁文創園區","國家歌劇院","傳統藝術中心","清水斷崖","奇美博物館","六福村","test1","test2","test3","test4","test5","test6"};
 
     //@SuppressLint("WrongViewCast")
     @Override
@@ -26,15 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         grid_recycler = findViewById(R.id.GridViewRecycler);
-        layoutManager = new GridLayoutManager(MainActivity.this,2); // 每行兩個
+        layoutManager = new GridLayoutManager(MainActivity.this,4); // 每行兩個
         grid_recycler.setHasFixedSize(true); // 確保 RecyclerView 在增減 item 時，item 的寬高都不會改變
         grid_recycler.setLayoutManager(layoutManager);
 
         gridAdapter = new GridAdapter(MainActivity.this,iconId,nameList);
         grid_recycler.setAdapter(gridAdapter);
-
-        //存入圖片
-
-
     }
 }
--- app/src/main/java/com/example/slideimage_gridview_test/SmoothLinearLayoutManager.java
+++
@@ -1,4 +0,0 @@
-package com.example.slideimage_gridview_test;
-
-public class SmoothLinearLayoutManager {
-}
